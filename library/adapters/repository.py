3af99fe388f40c03c6ce7df2cb156780d230132e
Simon <simonktpalu@gmail.com>
2021-09-08 17:18:55 +1200

    Added simplest iteration of repository.py, added an image of the complexity of the COVID 19 web app, will delete before due date

---
+++ library/adapters/repository.py
@@ -0,0 +1,32 @@
+import abc
+
+from library.domain.model import Publisher, Author, Book
+
+repo_instance = None
+
+
+class RepositoryException(Exception):
+
+    def __init__(self, message=None):
+        pass
+
+
+class AbstractRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def get_book(self, book_id: int):
+        """Returns Book with id from Repository, return None if not there"""
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_first_book(self):
+        """Returns Book with lowest id from Repository"""
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_last_book(self):
+        """Returns Book with highest id from Repository"""
+
+        raise NotImplementedError
