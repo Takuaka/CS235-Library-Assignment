cb1ec230f0453e39b9d029e0ade98fac881beded
Simon <simonktpalu@gmail.com>
2021-09-09 02:45:03 +1200

    finished tests for earliest iteration of test_memory_repository.py

--- tests/unit/test_memory_repository.py
+++ tests/unit/test_memory_repository.py
@@ -5,19 +5,30 @@ from library.adapters.repository import RepositoryException
 
 
 def test_repository_can_get_book(in_memory_repo):
-    book = in_memory_repo.get_book(1)
-
-    pass
+    book = in_memory_repo.get_book(2)
+    assert book.book_id == 5251
+    assert book.title == "Skulduggery Pleasant"
 
 
 def test_repository_can_get_first_book(in_memory_repo):
     book = in_memory_repo.get_first_book()
-
-    pass
+    assert book.book_id == 4432
+    assert book.title == "The Secret Hour"
 
 
 def test_repository_can_get_last_book(in_memory_repo):
     book = in_memory_repo.get_last_book()
+    assert book.book_id == 6431
+    assert book.title == "StormFront"
 
-    pass
+
+def test_repository_can_add_book(in_memory_repo):
+    book = Book(9999, "The Mist")
+    in_memory_repo.add_book(book)
+    assert in_memory_repo.get_book(5) is book
+
+
+def test_repository_does_not_get_book_that_does_not_exist(in_memory_repo):
+    book = in_memory_repo.get_book(7)
+    assert book is None
 
