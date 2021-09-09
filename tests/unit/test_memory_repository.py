ff03a3b73aa17e5958dcc853c3a8205a6a6bb9f5
Simon <simonktpalu@gmail.com>
2021-09-09 14:35:29 +1200

    My tests are wrong; the books are sorted by book_id, and so will not follow the order that I've read them from the file; I want to change it so books can be searched alphabetically, but I will save that for another time.

--- tests/unit/test_memory_repository.py
+++ tests/unit/test_memory_repository.py
@@ -5,7 +5,7 @@ from library.adapters.repository import RepositoryException
 
 
 def test_repository_can_get_book(in_memory_repo):
-    book = in_memory_repo.get_book(2)
+    book = in_memory_repo.get_book(5251)
     assert book.book_id == 5251
     assert book.title == "Skulduggery Pleasant"
 
@@ -25,10 +25,10 @@ def test_repository_can_get_last_book(in_memory_repo):
 def test_repository_can_add_book(in_memory_repo):
     book = Book(9999, "The Mist")
     in_memory_repo.add_book(book)
-    assert in_memory_repo.get_book(5) is book
+    assert in_memory_repo.get_book(9999) is book
 
 
 def test_repository_does_not_get_book_that_does_not_exist(in_memory_repo):
-    book = in_memory_repo.get_book(7)
+    book = in_memory_repo.get_book(4656)
     assert book is None
 
