91c3c4711b84ee14a460f161f6438be694a3eb0f
Simon <simonktpalu@gmail.com>
2021-09-24 00:48:34 +1200

    I think the e2e response.data can't provide the table I generated for the books page because of the for loop. Oh well, at least I know it works properly.

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -14,3 +14,13 @@ def test_books(client):
     assert response.status_code == 200
     assert b'Book Title' in response.data
     assert b'Book ID' in response.data
+
+
+def test_book_details(client):
+    response = client.get('/book_details/4432')
+    assert response.status_code == 200
+    assert b'The Secret Hour' in response.data
+    assert b'Publisher: HarperCollins' in response.data
+    assert b'Book ID: 4432' in response.data
+    assert b'Not available as eBook' in response.data
+    assert b'Page number: 297' in response.data
