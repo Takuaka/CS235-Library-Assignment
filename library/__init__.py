7552a0441e0d3c512baa65cd09eefe2e7cde559a
Simon <simonktpalu@gmail.com>
2021-10-11 16:42:22 +1300

    __init__.py has been modified to take in database values; next up is db tests and modifiying adapters to take these into account

--- library/__init__.py
+++ library/__init__.py
@@ -4,8 +4,13 @@ from pathlib import Path
 
 from flask import Flask
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.pool import NullPool
+
 import library.adapters.repository as repo
-from library.adapters.memory_repository import MemoryRepository, populate
+from library.adapters import memory_repository, database_repository, repository_populate
+from library.adapters.orm import metadata, map_model_to_tables
 
 
 def create_app(test_config=None):
@@ -18,8 +23,40 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
         data_path = app.config['TEST_DATA_PATH']
 
-    repo.repo_instance = MemoryRepository()
-    populate(data_path, repo.repo_instance)
+# Choosing either memory Repo or database Repo
+
+    if app.config['REPOSITORY'] == 'memory':
+        repo.repo_instance = memory_repository.MemoryRepository()
+        database_mode = False
+        repository_populate.populate(data_path, repo.repo_instance, database_mode)
+
+    elif app.config['REPOSITORY'] == 'database':
+        database_uri = app.config['SQLALCHEMY_DATABASE_URI']
+
+        database_echo = app.config['SQLALCHEMY_ECHO']
+
+        database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool, echo=database_echo)
+
+        session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
+
+        repo.repo_instance = database_repository.SqlAlchemyRepository(session_factory)
+
+        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+            print("REPOPULATING DATABASE...")
+            # reinitialising database for testing or for first-time use of application
+            clear_mappers()
+            metadata.create_all(database_engine)
+            for table in reversed(metadata.sorted_tables):
+                database_engine.execute(table.delete())
+
+            map_model_to_tables()
+
+            database_mode = True
+            repository_populate.populate(data_path, repo.repo_instance, database_mode)
+            print("REPOPULATING DATABASE... FINISHED")
+
+        else:
+            map_model_to_tables()
 
     with app.app_context():
         from .home import home
@@ -30,5 +67,17 @@ def create_app(test_config=None):
 
         from .authors import authors
         app.register_blueprint(authors.authors_blueprint)
+
+        # These are for the database repo; not for the memory repo
+
+        @app.before_request
+        def before_flask_http_request_function():
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.reset_session()
+
+        @app.teardown_appcontext
+        def shutdown_session(exception=None):
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.close_session()
 
     return app
