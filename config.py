c3d5cc2fb61b758bba3df1c404ce1b7daf0ea55f
Simon <simonktpalu@gmail.com>
2021-10-11 15:38:42 +1300

    Changed config.py and .env to accommodate for the database change

--- config.py
+++ config.py
@@ -13,3 +13,12 @@ class Config:
 
     TESTING = environ.get('TESTING')
 
+    REPOSITORY = environ.get('REPOSITORY')
+
+    #Database configuration
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+
+    echo_string = environ.get('SQLALCHEMY_ECHO')
+    SQLALCHEMY_ECHO = False
+    if echo_string.lower().strip() == "true":
+        SQLALCHEMY_ECHO = True
